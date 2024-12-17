8ff15c4a50f20c9eb00bacc97f3f3be051ff151c
nia <1205367274@qq.com>
2024-12-18 00:33:13 +0800

    update

--- publish.ps1
+++ publish.ps1
@@ -1,34 +1,56 @@
-# filepath: publish.ps1
-
-$bookDir = $env:MDBOOK_DIR
+$bookDir = (Get-Location).Path
 $publishBranch = "gh-pages"
 $buildDir = "$bookDir\book"
 
-
-if (-Not $bookDir) {
-    Write-Host "the env MDBOOK_DIR lost" -ForegroundColor Red
-    exit 1
-}
+git stash push -u -m "Stash before switching branches"
 
 Set-Location $bookDir
 
 mdbook build
+
 if (-Not (Test-Path $buildDir)) {
-    Write-Host "mdbook build error" -ForegroundColor Red
+    Write-Host "Build mdbook error" -ForegroundColor Red
+    git stash pop
     exit 1
 }
 
 git checkout $publishBranch
 
+$currentBranch = git rev-parse --abbrev-ref HEAD
+if ($currentBranch -ne $publishBranch) {
+    Write-Host "Failed to switch to branch '$publishBranch'. Reverting changes." -ForegroundColor Red
+    git stash pop
+    exit 1
+} else {
+    Write-Host "Successfully switched to branch '$publishBranch'." -ForegroundColor Green
+}
 
-Copy-Item -Path "$buildDir\*" -Destination $bookDir -Recurse -Force
+Copy-Item -Path "$buildDir\*" -Destination $bookDir -Recurse -Force -Exclude "publish.ps1"
 
 git add .
+git commit -m "Update GitHub Pages" | Out-Null
 
-git commit -m "update GitHub Pages"
-
-git push origin $publishBranch --force
+try {
+    git push origin $publishBranch --force
+} catch {
+    Write-Host "Failed to push to '$publishBranch' branch. Restoring state." -ForegroundColor Red
+    git checkout main
+    git stash pop
+    exit 1
+}
 
 git checkout main
 
-Write-Host "mdbook publish to gh-pages branch success" -ForegroundColor Green
+$currentBranch = git rev-parse --abbrev-ref HEAD
+if ($currentBranch -ne "main") {
+    Write-Host "Failed to switch back to 'main' branch. Please check manually." -ForegroundColor Red
+    git stash pop
+    exit 1
+} else {
+    Write-Host "Successfully switched back to 'main' branch." -ForegroundColor Green
+}
+
+git stash pop
+
+
+Write-Host "Successfully publish mdbook to gh-pages branch" -ForegroundColor Green
--- src/top.md
+++ src/top.md
@@ -4,6 +4,8 @@
 
 但实际上这里并没有什么特别的
 
+---
+
 Vous pouvez penser que vous avez trouvé quelque chose d’extraordinaire
 
 Mais il n’y a rien d’extraordinaire ici
